f3786c168df6c76a27a249c5f2d79040312527a7
danielhep <daniel.heppner1@gmail.com>
2017-03-04 22:23:23 -0800

    clean up

--- DoorMachine.ino
+++ DoorMachine.ino
@@ -84,16 +84,6 @@ void setup() {
 
                      // initialize the lcd
   lcd.init();
-  // Print a message to the LCD.
-  lcd.backlight();
-  lcd.setCursor(3, 0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2, 1);
-  lcd.print("Ywrobot Arduino!");
-  lcd.setCursor(0, 2);
-  lcd.print("Arduino LCM IIC 2004");
-  lcd.setCursor(2, 3);
-  lcd.print("Power By Ec-yuan!");
 }
 
 void loop() {
