c6eb41d60b07ed4f54be41b207629aff327af127
danielhep <daniel.heppner1@gmail.com>
2017-03-10 00:54:32 -0800

    more clean up

--- DoorMachine.ino
+++ DoorMachine.ino
@@ -29,12 +29,13 @@
 #define EFFECT_WHEEL 1
 #define EFFECT_WATER 2
 
-int irqpin = D3;  // Digital 3
-int REDPIN = D6;
-int GREENPIN = D5;
-int BLUEPIN = D7;
+#define REDPIN D6
+#define GREENPIN D5
+#define BLUEPIN D7
 
-int MOTION_PIN = D8;
+#define IRQ_PIN D3
+
+#define MOTION_PIN D8
 
 int ledStripEffect = 0;
 int effectStrength = 255;
@@ -55,15 +56,28 @@ WiFiServer server(80);
 LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup() {
-  pinMode(irqpin, INPUT);
-  digitalWrite(irqpin, HIGH); //enable pullup resistor
-
-  pinMode(MOTION_PIN, INPUT);
-
+  // Setup serial and i2c
   Serial.begin(9600);
   Wire.begin();
 
-  WiFi.mode(WIFI_STA);
+  // Setup trigger pin for capacitive sensor
+  pinMode(IRQ_PIN, INPUT);
+  digitalWrite(IRQ_PIN, HIGH); //enable pullup resistor
+
+  // Setup motion detector pin
+  pinMode(MOTION_PIN, INPUT);
+
+
+  // Setup the 3 PWM outputs for the LED strip
+  pinMode(REDPIN, OUTPUT);
+  pinMode(GREENPIN, OUTPUT);
+  pinMode(BLUEPIN, OUTPUT);
+
+  // Let us turn on the built in LED!
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // Connect to WiFi
+  WiFi.mode(WIFI_STA); // client mode
   WiFi.begin("WWUResTek-alt");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
@@ -75,14 +89,9 @@ void setup() {
   Serial.println(WiFi.localIP());
   server.begin();
 
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  pinMode(REDPIN, OUTPUT);
-  pinMode(GREENPIN, OUTPUT);
-  pinMode(BLUEPIN, OUTPUT);
   mpr121_setup();
 
-                     // initialize the lcd
+  // initialize the lcd
   lcd.init();
 }
 
@@ -116,140 +125,6 @@ void checkMotionDetector() {
     }
   }
 }
-
-void readTouchInputs() {
-  if (!checkInterrupt()) {
-    //read the touch state from the MPR121
-    Wire.requestFrom(0x5A, 2);
-
-    byte LSB = Wire.read();
-    byte MSB = Wire.read();
-
-    uint16_t touched = ((MSB << 8) | LSB); //16bits that make up the touch states
-
-    for (int i = 0; i < 12; i++) { // Check what electrodes were pressed
-      if (touched & (1 << i)) { // i is being touches
-
-        if (touchStates[i] == false) { // It was not set as touched already
-          //pin i was just touched
-          Serial.print("pin ");
-          Serial.print(i);
-          Serial.println(" was just touched");
-          if (i == 0) {
-            peepholeTouchCount++;
-
-            if (nightMode)
-              setColor(255, 0, 0, 0, 500);
-            else
-              setEffect(EFFECT_WHEEL, 255, 900);
-          }
-
-        } else if (touchStates[i] == 1) {
-          //pin i is still being touched
-        }
-
-        touchStates[i] = true;      // Set it as touched
-      } else { // i is not being touched
-        if (touchStates[i] == true) { // if it was set as touched before
-          Serial.print("pin ");
-          Serial.print(i);
-          Serial.println(" is no longer being touched");
-
-          if (i == 0) {
-            if (!nightMode)
-              setColor(255, 255, 255, 2000, 200);
-            else
-              setColor(100, 0, 0, 3500, 500);
-          }
-
-          //pin i is no longer being touched
-        }
-        touchStates[i] = false;
-      }
-
-    }
-  }
-}
-
-void mpr121_setup(void) {
-
-  set_register(0x5A, ELE_CFG, 0x00);
-
-  // Section A - Controls filtering when data is > baseline.
-  set_register(0x5A, MHD_R, 0x01);
-  set_register(0x5A, NHD_R, 0x01);
-  set_register(0x5A, NCL_R, 0x00);
-  set_register(0x5A, FDL_R, 0x00);
-
-  // Section B - Controls filtering when data is < baseline.
-  set_register(0x5A, MHD_F, 0x01);
-  set_register(0x5A, NHD_F, 0x01);
-  set_register(0x5A, NCL_F, 0xFF);
-  set_register(0x5A, FDL_F, 0x02);
-
-  // Section C - Sets touch and release thresholds for each electrode
-  set_register(0x5A, ELE0_T, TOU_THRESH);
-  set_register(0x5A, ELE0_R, REL_THRESH);
-
-  set_register(0x5A, ELE1_T, TOU_THRESH);
-  set_register(0x5A, ELE1_R, REL_THRESH);
-
-  set_register(0x5A, ELE2_T, TOU_THRESH);
-  set_register(0x5A, ELE2_R, REL_THRESH);
-
-  set_register(0x5A, ELE3_T, TOU_THRESH);
-  set_register(0x5A, ELE3_R, REL_THRESH);
-
-  set_register(0x5A, ELE4_T, TOU_THRESH);
-  set_register(0x5A, ELE4_R, REL_THRESH);
-
-  set_register(0x5A, ELE5_T, TOU_THRESH);
-  set_register(0x5A, ELE5_R, REL_THRESH);
-
-  set_register(0x5A, ELE6_T, TOU_THRESH);
-  set_register(0x5A, ELE6_R, REL_THRESH);
-
-  set_register(0x5A, ELE7_T, TOU_THRESH);
-  set_register(0x5A, ELE7_R, REL_THRESH);
-
-  set_register(0x5A, ELE8_T, TOU_THRESH);
-  set_register(0x5A, ELE8_R, REL_THRESH);
-
-  set_register(0x5A, ELE9_T, TOU_THRESH);
-  set_register(0x5A, ELE9_R, REL_THRESH);
-
-  set_register(0x5A, ELE10_T, TOU_THRESH);
-  set_register(0x5A, ELE10_R, REL_THRESH);
-
-  set_register(0x5A, ELE11_T, TOU_THRESH);
-  set_register(0x5A, ELE11_R, REL_THRESH);
-
-  // Section D
-  // Set the Filter Configuration
-  // Set ESI2
-  set_register(0x5A, FIL_CFG, 0x04);
-
-  // Section E
-  // Electrode Configuration
-  // Set ELE_CFG to 0x00 to return to standby mode
-  set_register(0x5A, ELE_CFG, 0x0C);  // Enables all 12 Electrodes
-
-
-  // Section F
-  // Enable Auto Config and auto Reconfig
-  /*set_register(0x5A, ATO_CFG0, 0x0B);
-    set_register(0x5A, ATO_CFGU, 0xC9);  // USL = (Vdd-0.7)/vdd*256 = 0xC9 @3.3V   set_register(0x5A, ATO_CFGL, 0x82);  // LSL = 0.65*USL = 0x82 @3.3V
-    set_register(0x5A, ATO_CFGT, 0xB5);*/  // Target = 0.9*USL = 0xB5 @3.3V
-
-  set_register(0x5A, ELE_CFG, 0x0C);
-
-}
-
-
-boolean checkInterrupt(void) {
-  return digitalRead(irqpin);
-}
-
 
 void set_register(int address, unsigned char r, unsigned char v) {
   Wire.beginTransmission(address);
